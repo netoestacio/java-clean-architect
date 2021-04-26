109b8f622f4d9136fff3ae0ec90502625616c12e
netoestacio <pedronetoestacio@gmail.com>
2021-04-26 20:42:03 -0300

    ,..

---
+++ src/test/java/br/com/clean/CleanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
